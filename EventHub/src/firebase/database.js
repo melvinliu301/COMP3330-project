eab0a58758c403028b3e43c24b378231f00526f6
Melvin Liu <u3579073@connect.hku.hk>
2023-11-18 04:16:52 +0800

    Add profile and MyEvents
    add profile skeleton, and get "My Events"

--- EventHub/src/firebase/database.js
+++ EventHub/src/firebase/database.js
@@ -1,17 +1,7 @@
 import { app } from "./base";
-import {
-    getFirestore,
-    collection,
-    doc,
-    getDocs,
-    addDoc,
-    setDoc,
-    updateDoc,
-} from "firebase/firestore";
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { getFirestore, collection, doc, getDocs, addDoc, setDoc, updateDoc, getDoc } from "firebase/firestore";
 
 const db = getFirestore(app);
-const storage = getStorage();
 
 const addData = async (collectionName, data) => {
     const docRef = await addDoc(collection(db, collectionName), data);
@@ -35,19 +25,19 @@ const getData = async (collectionName) => {
     return querySnapshot.docs;
 };
 
+const getDataById = async (collectionName, id) => {
+    const docRef = doc(db, collectionName, id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        return docSnap.data();
+    } else{
+        return null;
+    }
+}
+
 const updateData = async (collectionName, id, data) => {
     const docRef = await updateDoc(doc(db, collectionName, id), data);
     return docRef.id;
-};
+}
 
-const getBinaryURL = async (path) => {
-    const starsRef = ref(storage, path);
-    try {
-        const url = await getDownloadURL(starsRef);
-        return url;
-    } catch (err) {
-        return null;
-    }
-};
-
-export { addData, setData, getData, updateData, getBinaryURL };
+export {addData, setData, getData, updateData, getDataById };
--- EventHub/src/screens/SettingScreen.jsx
+++ EventHub/src/screens/SettingScreen.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, View, Button, TouchableHighlight, Pressable, Modal, Text, FlatList } from "react-native";
+import { StyleSheet, View, Button, TouchableHighlight, Pressable, Text, FlatList } from "react-native";
 import { auth } from "../firebase/auth";
 import {
     onAuthStateChanged,
@@ -7,10 +7,16 @@ import {
     signOut,
 } from "firebase/auth";
 import { ALLOWED_EMAILS } from "../common/constants";
+import Dialog, { DialogContent } from "react-native-popup-dialog";
+import {getData, getDataById} from '../firebase/database';
+
 
 const SettingScreen = () => {
+
     const [user, setUser] = useState(null);
-    const [modalVisible, setModalVisible] = useState(false);
+    const [myEventVisible, setmyEventVisible] = useState(false);
+    const [myEventsID, setmyEventsID] = useState([]);
+    const [myEvents, setmyEvents] = useState([]);
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -23,6 +29,24 @@ const SettingScreen = () => {
         });
     }, []);
 
+    useEffect(() => {
+        getDataById("Users", auth.currentUser.uid).then((data) => {
+            setmyEventsID(data.events);
+        });
+
+    }, {});
+
+    useEffect( () => {
+        const tempArray = [];
+        for (let i = 0; i < myEventsID.length; i++) {
+            getDataById("Events", myEventsID[i]).then((data) => {
+                tempArray.push(data);
+            });
+        }
+        setmyEvents(tempArray);
+        console.log(tempArray);
+    }, [myEventsID]);
+
     const handleSignOut = async () => {
         await signOut(auth);
     };
@@ -31,49 +55,77 @@ const SettingScreen = () => {
         await sendEmailVerification(user);
     };
 
+    const editProfile = () => {
+        console.log("edit profile");
+    }
 
     return (
         <View style={styles.container}>
-            <Modal
-                animationType="fade"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                  setModalVisible(!modalVisible);
-                }}>
-                    <View style={styles.modalView}>
-                        <View style={styles.infoView}>
-                            <View>
-                                <Text>You are not holding an event. Go and organize one!</Text>
-                            </View>
-                            
-                            <Pressable
-                                style={[styles.closebutton]}
-                                onPress={() => setModalVisible(!modalVisible)}>
-                                {modalVisible && <Text>Close</Text>}
-                            </Pressable>
-                        </View>
-                    </View>
-            </Modal>
+
+
+            <Dialog
+                visible={myEventVisible}
+                onTouchOutside={() => {setmyEventVisible(false);}}
+                height={"50%"}
+                >
+                
+                <DialogContent>
+                    <View style={styles.dialogView}>
+                        {
+                            myEvents.length > 0?
+                            <FlatList
+                                data={myEvents}
+                                style = {{flex:1, }}
+                                renderItem={({ item }) => (
+                                    <View style={{borderWidth: 1, borderColor: "gray", borderRadius: 10, margin: 10}}>
+                                        <Text style={{fontSize: 24, fontWeight: "bold"}}>{item.title}</Text>
+                                        <Text style={{fontSize: 16}}>{item.description}</Text>
+                                        <Text style={{fontSize: 16}}>Date: {item.date}</Text>
+                                        <Text style={{fontSize: 16}}>Time: {item.startTime} - {item.endTime}</Text>
+                                        <Text style={{fontSize: 16}}>Location: {item.location}</Text>
+                                        <Text style={{fontSize: 16}}>Category: {item.category}</Text>
+                                    </View>
+                                )}
+                                keyExtractor={(item) => item.id}
+                            />
+                            :<Text>You are not holding an event. Go and organize one!</Text>
+                        }
+                    
+                    
+                    </View> 
+                </DialogContent>
+            </Dialog>
+
 
             {/*my profile*/}
             <View style={styles.myProfileView}>
                 <Text style={{fontSize:24, fontWeight: "bold"}}>My profile</Text>
                 <View style={{ flexDirection: "row" }}>
-                    <View style={{margin:10}}>
+                    <View style={{margin:10, flex:1}}>
                         <Text>any profile pic</Text>
                     </View>
-                    <View>
-                        <Text style={styles.text}>any username</Text>
-                        <Text style={styles.text}>any email</Text>
+                    <View style={{flex:3}}>
+                        {auth.currentUser.displayName?<Text style={styles.text}>{auth.currentUser.displayName}</Text>
+                        :<Text style={[styles.text,{fontStyle: "italic"}]}>Go give yourself a name!</Text>}
+                        <Text style={styles.text}>{auth.currentUser.email}</Text>
                     </View>
+
+
+                    <Pressable style={styles.editProfileButton} onPress={editProfile}>
+                        <Text style={styles.editProfileButtonText}>Edit</Text>
+                    </Pressable>
+
                 </View>
             </View>
+
+            {/*view my events*/}
             <View>
-                <Pressable style={styles.viewMyEventButton} onPress={() => setModalVisible(true)}>
+                <Pressable style={styles.viewMyEventButton} onPress={() => setmyEventVisible(true)}>
                     <Text style={styles.viewMyEventButtonText}>View My Events</Text>
                 </Pressable>
             </View>
+
+            {/*verify email*/}
             {user &&
                 !user.emailVerified &&
                 ALLOWED_EMAILS.includes(user.email || "") && (
@@ -86,6 +138,8 @@ const SettingScreen = () => {
                         />
                     </TouchableHighlight>
                 )}
+
+            {/*sign out*/}
             <TouchableHighlight style={styles.touchableHighlight}>
                 <Button
                     title="Sign out"
@@ -129,36 +183,27 @@ const styles = StyleSheet.create({
         fontSize: 24,
         textDecorationLine: "underline",
     },
-    modalView: {
+    dialogView: {
+        // margin: 10,
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        transparent: true,
-        backgroundColor: "rgba(0,0,0,0.5)",
-      },
-      infoView: {
-        alignSelf: 'center',
-        borderWidth: 2,
-        borderColor: "orange",
-        backgroundColor: "white",
-        margin: 20,
-        padding: 35,
-        alignItems: 'center',
-        // shadowColor: '#000',
-        // shadowOffset: {
-        //   width: 0,
-        //   height: 2,
-        // },
-        // shadowOpacity: 0.25,
-        // shadowRadius: 4,
-        // elevation: 5,
-      },
-      closebutton: {
-        backgroundColor: "orange",
-        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    editProfileButton: {
+        alignItems: "center",
         padding: 10,
-        elevation: 2,
-      },
+        margin:10, 
+        flex:1, 
+        alignSelf:'center',
+        borderRadius: 10,
+        backgroundColor: "orange",
+    },
+    editProfileButtonText: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "white",
+    }
+
 });
 
 export default SettingScreen;
