38075823ab2f3700ff4753647833a051439f0553
JohnTseung <u3578708@connect.hku.hk>
2023-11-18 22:16:33 +0800

    Fix header, two arrays for users database

--- EventHub/src/screens/LoginRegisterScreens.jsx
+++ EventHub/src/screens/LoginRegisterScreens.jsx
@@ -56,7 +56,8 @@ const SignUpScreen = ({ navigation }) => {
 
             await setData("Users", userCredential.user.uid, {
                 username: username,
-                events: [],
+                joinedEvents: [],
+                createdEvents: [],
             });
 
             setSignUpSuccess(true);
